9a085b5f91bad88abc61275b13978a702c92b5c1
thevinodpatidar <pvinod813@gmail.com>
2024-12-26 21:25:11 +0530

    Remove unused components and assets; add NotFound page
    - Deleted the icon image and several layout and page components related to cookies policy, privacy policy, terms and conditions, and thank you pages, streamlining the application structure.
    - Introduced a new NotFound page to handle 404 errors, enhancing user experience by providing a clear message when a page is not found.
    - Updated the layout.tsx file to remove unnecessary metadata and imports, simplifying the codebase.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,5 @@
-import type { Metadata } from "next";
-import { getSiteConfig } from "@/lib/config";
-import { geistSans, geistMono } from "./fonts";
 import "@/app/globals.css";
-
-export const metadata: Metadata = {
-  title: getSiteConfig("/").seo.title,
-  description: getSiteConfig("/").seo.description,
-  keywords: getSiteConfig("/").seo.keywords,
-  openGraph: {
-    images: [getSiteConfig("/").branding.ogImage],
-  },
-};
+import { geistMono, geistSans } from "./fonts";
 
 export default function RootLayout({
   children,
